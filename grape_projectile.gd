af0dc90b974831a060bc55c18f9fa93a04c4a2c2
Spenny <spencer.stirling00@gmail.com>
2024-11-23 16:22:17 -0700

    progress on adding plant level-up abilities

--- grape_projectile.gd
+++ grape_projectile.gd
@@ -7,20 +7,20 @@ signal returned(projectile)
 
 # defines functions for adding and returning projectiles upon collision
 func _ready():
-    body_entered.connect(_on_body_entered) 
-    
+	body_entered.connect(_on_body_entered) 
+	
 func _physics_process(delta):
-    position += linear_velocity * delta  
-    
+	position += linear_velocity * delta  
+	
 func add_to_pool():
-    hide() 
-    linear_velocity = Vector2.ZERO 
-    position = Vector2.ZERO
-    
+	hide() 
+	linear_velocity = Vector2.ZERO 
+	position = Vector2.ZERO
+	
 func _on_body_entered(_body):
-    linear_velocity = Vector2.ZERO
-    returned.emit(self)
-    add_to_pool()
+	linear_velocity = Vector2.ZERO
+	returned.emit(self)
+	add_to_pool()
 
 func get_projectile_damage():
-    null
+	null
