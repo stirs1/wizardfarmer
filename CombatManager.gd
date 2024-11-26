ca8fc0450cc6bc227961f3c9bffc774866220326
Spenny <spencer.stirling00@gmail.com>
2024-11-26 13:52:44 -0700

    created a game started state

--- CombatManager.gd
+++ CombatManager.gd
@@ -1,25 +1,14 @@
 extends Node
 
-var pumpkin: Node2D
+#var pumpkin: Node2D
 var cm_dtag := "COMBAT MANAGER: "
-@onready var PumpkinScene = preload("res://enemy_pumpkin.tscn")
+
+#@onready var PumpkinScene = preload("res://enemy_pumpkin.tscn")
 
 func _ready():
-	await get_tree().root.ready
-	print(cm_dtag, "Connecting to game_started signal...")
-	GameManager.game_started.connect(_on_game_started)
-	print(cm_dtag, "Connected!")
-
+    await get_tree().root.ready
+    GameManager.game_started.connect(_on_game_started)
+ 
 func _on_game_started():
-	print(cm_dtag, "running _on_game_started...")
-	var main = get_tree().root
-	if main:
-		print(cm_dtag, "main exists")
-		pumpkin = PumpkinScene.instantiate()
-		print(cm_dtag, "pumpkin instantiated")
-		main.add_child.call_deferred(pumpkin)  # using call_deferred to be safe
-		pumpkin.position = Vector2(400, 400)
-		print(cm_dtag, "pumpkin instance created at ", pumpkin.position)
-	else:
-		print(cm_dtag, "main not found")
-	
+    print(cm_dtag, "signal received!")
+    
--- Global.gd
+++ Global.gd
@@ -10,7 +10,7 @@ const tile_offset = tile_size / 2
 @onready var PlantPots = get_tree().get_nodes_in_group("PlantPots")
 # not sure this is what i want here, keeping for now
 #@onready var PlantGrapeshotArea2D = get_tree().root.get_node(
-	#"Main/Plant_Grapeshot/Plant_Grapeshot_Area2D")
+    #"Main/Plant_Grapeshot/Plant_Grapeshot_Area2D")
 @onready var settings = preload("res://settings.tres")
 
 # defined containers assigned to their correct type
@@ -30,83 +30,83 @@ var plant_count = 0
 
 # FIXME: doesn't always do what i want and i don't know why
 func snap_to_grid(position: Vector2) -> Vector2:
-	var new_position = Vector2.ZERO
-	new_position.x = (
-		(round(position.x / tile_size) * tile_size) + tile_offset
-		)
-	new_position.y = (
-		(round(position.y / tile_size) * tile_size) + tile_offset
-		)
-	return new_position
+    var new_position = Vector2.ZERO
+    new_position.x = (
+        (round(position.x / tile_size) * tile_size) + tile_offset
+        )
+    new_position.y = (
+        (round(position.y / tile_size) * tile_size) + tile_offset
+        )
+    return new_position
 
 func _ready():
-	
-	await get_tree().root.ready # makes sure to wait for tree to be loaded?
-		
-	# adds the wizard to the scene, ai helped with this so idk what's goin on
-	wizard = get_tree().root.get_node("Main/wizard")
-	if not wizard:
-		wizard = WizardScene.instantiate()
-		add_child(wizard)
-		print("Created new wizard instance")
-	 
-	# set wizard start cell right here. 
-	# FIXME: something weird happening with the wizard's origin point
-	start_cell = snap_to_grid(
-		Vector2(7 * tile_size, 7 * tile_size)) + Vector2(13, 13
-		)
+    
+    await get_tree().root.ready # makes sure to wait for tree to be loaded?
+        
+    # adds the wizard to the scene, ai helped with this so idk what's goin on
+    wizard = get_tree().root.get_node("Main/wizard")
+    if not wizard:
+        wizard = WizardScene.instantiate()
+        add_child(wizard)
+        print("Created new wizard instance")
+     
+    # set wizard start cell right here. 
+    # FIXME: something weird happening with the wizard's origin point
+    start_cell = snap_to_grid(
+        Vector2(7 * tile_size, 7 * tile_size)) + Vector2(13, 13
+        )
 
-	# this makes sure the wizard is on the correct cell AND centered
-	wizard.position = start_cell 
-	
-	# setting plant positions
-	# room here for improvement/more robust logic
-	for plant in Global.PlantPots:
-		plant_start_cell = snap_to_grid(
-			Vector2(6 * tile_size, (6 + plant_count) * tile_size))
-		plant.position = plant_start_cell + (Vector2(0, tile_size))
-		plant_count += 1 
-		
-	# this defines the no grow zones. This is hard-coded, but i think there's a
-	# way to just loop from starting coordinates to end by adding tile_size?
-	no_grow_zones = {
-		
-		# zone 1
-		"(368, 304)": "true",
-		"(400, 304)": "true",
-		"(432, 304)": "true",
-		"(464, 304)": "true",
-		
-		# zone 2
-		"(528, 304)": "true",
-		"(560, 304)": "true",
-		"(592, 304)": "true",
-		"(624, 304)": "true",
-		
-		# zone 3
-		"(496, 176)": "true",
-		"(496, 208)": "true",
-		"(496, 240)": "true",
-		"(496, 272)": "true",
-		
-		# zone 4
-		"(496, 336)": "true",
-		"(496, 368)": "true",
-		"(496, 400)": "true",
-		"(496, 432)": "true",
-		
-		# heart home
-		"(496, 304)": "true",
-		
-		# plant origins
-		"(208, 240)": "true",
-		"(208, 272)": "true",
-		"(208, 304)": "true",
-		"(208, 336)": "true"
-	}
-	
+    # this makes sure the wizard is on the correct cell AND centered
+    wizard.position = start_cell 
+    
+    # setting plant positions
+    # room here for improvement/more robust logic
+    for plant in Global.PlantPots:
+        plant_start_cell = snap_to_grid(
+            Vector2(6 * tile_size, (6 + plant_count) * tile_size))
+        plant.position = plant_start_cell + (Vector2(0, tile_size))
+        plant_count += 1 
+        
+    # this defines the no grow zones. This is hard-coded, but i think there's a
+    # way to just loop from starting coordinates to end by adding tile_size?
+    no_grow_zones = {
+        
+        # zone 1
+        "(368, 304)": "true",
+        "(400, 304)": "true",
+        "(432, 304)": "true",
+        "(464, 304)": "true",
+        
+        # zone 2
+        "(528, 304)": "true",
+        "(560, 304)": "true",
+        "(592, 304)": "true",
+        "(624, 304)": "true",
+        
+        # zone 3
+        "(496, 176)": "true",
+        "(496, 208)": "true",
+        "(496, 240)": "true",
+        "(496, 272)": "true",
+        
+        # zone 4
+        "(496, 336)": "true",
+        "(496, 368)": "true",
+        "(496, 400)": "true",
+        "(496, 432)": "true",
+        
+        # heart home
+        "(496, 304)": "true",
+        
+        # plant origins
+        "(208, 240)": "true",
+        "(208, 272)": "true",
+        "(208, 304)": "true",
+        "(208, 336)": "true"
+    }
+    
 func _physics_process(delta: float) -> void:
-		  
-	# defining wizard global position and grid coordinates
-	wizard_position = wizard.global_position
-	wizard_cell_position = round(wizard_position / tile_size)
+          
+    # defining wizard global position and grid coordinates
+    wizard_position = wizard.global_position
+    wizard_cell_position = round(wizard_position / tile_size)
