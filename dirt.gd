90633632108a246a68df82bd532555f6f95455d7
Spenny <spencer.stirling00@gmail.com>
2024-12-02 23:40:34 -0700

    added 4 water levels

--- dirt.gd
+++ dirt.gd
@@ -3,17 +3,18 @@ extends StaticBody2D
 signal water_state_changed(has_water: bool)
 
 @onready var dirt_sprite = $dirt_sprite
-@export var water_level: int = 4:
-	set(value):
+
+var water_level: int = 4
+	
+func set_water_level(new_value: int) -> void:
 		var was_watered: bool = water_level > 0
-		water_level = clampi(value, 0, 4)
+		water_level = clampi(new_value, 0, 4)
 		var is_watered: bool = water_level > 0
 		
-		if dirt_sprite:
-			dirt_sprite.frame = water_level
-			
+		dirt_sprite.frame = water_level
+		
 		if was_watered != is_watered:
-				water_state_changed.emit(is_watered)
+			water_state_changed.emit(is_watered)
 			
 func has_water() -> bool:
 	return water_level > 0
