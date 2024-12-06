e906fa62eb5df0f6a5e704a7ba4c0989bf4922c8
Spenny <spencer.stirling00@gmail.com>
2024-12-05 22:34:34 -0700

    adds reference material, future water content

---
+++ water_plops.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+# HMM... perfectin is teh enmey of prgres
+
+# VARIABLES - - - - - - - - - - - - - - - - - - - - - - - - - - - - VARIABLES #
+var existing_water_tiles = {}
+var dry_tiles = {}
+var wdrop: PackedScene
+var held_wdrop: PackedScene
+# FUNCTIONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - FUNCTIONS #
+func drop_wdrop():
+	pass
+
+func moveto_wsource():
+	pass
+
+func find_wsource():
+	pass
+
+func find_dry():
+	pass
+
+func dry_sort():
+	pass
